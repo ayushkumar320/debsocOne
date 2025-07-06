e12f476fd0b1d2e70f9ab952431afb56a4b1eaea
mobasshir <mobasshirkhan9931@gmail.com>
2025-07-06 21:48:53 +0530

    integrated components into App

--- Frontend/DebateTimer/src/components/app.jsx
+++ Frontend/DebateTimer/src/components/app.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Description from "./description";
+import Card from "./card";
 
 function App() {
   return (
     <div>
       <Description />
+      <Card />
     </div>
   );
 }
 
+ 
 export default App;
---
+++ Frontend/DebateTimer/src/components/card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Clock from "./clock";
+
+function Card() {
+  return (
+    <div className="card">
+    <Clock />
+    </div>
+  );
+}
+
+export default Card;
