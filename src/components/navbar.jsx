11e78ff02a2921e4a3ea0b2f62d562586194c33b
mobasshir <mobasshirkhan9931@gmail.com>
2025-07-11 23:04:21 +0530

    init the navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Navbar() {
+    return (
+        <nav>
+            <h1>SMVIT DEBSOC</h1>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#events">Events</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
