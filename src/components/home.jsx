4bea6ab343d90d2c37dd498996090dde031071a7
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-13 23:20:08 +0530

    update home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,7 +5,7 @@ function Home() {
     <div className="home">
         <h1 className="text-4xl font-bold  mt-10">Welcome to SMVIT DEBSOC</h1>
 
-        <img src="./media/M5.jpg" alt="group photo" className="rounded-lg shadow-md mt-4  h-[800px] w-[800px] relative right-0 ml-auto mt-4 shadow-md block " />
+        <img src="./media/M5.jpg" alt="group photo" className="rounded-lg shadow-md h-[800px] w-[800px] relative right-0 ml-auto mt-4 block " />
       
     </div>
   );
