56e9a613e4eda97a2192159b101ac824da9105b3
mobasshir <mobasshirkhan9931@gmail.com>
2025-07-17 22:15:01 +0530

    added lenis to make smooth scroll

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,20 @@ import AchievementsCarousel from "./AchievementsCarousel";
 import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
 function App() {
     return (
+            <ReactLenis
+            root
+            options={{
+                duration: 1.5,
+                easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+                direction: 'vertical',
+                gestureDirection: 'vertical',
+                smooth: true,
+                mouseMultiplier: 1,
+                smoothTouch: true,
+                touchMultiplier: 2,
+                infinite: false,
+            }}
+        >
         <div>
            <Navbar />
             <Home />
@@ -19,6 +33,7 @@ function App() {
            <Footer />
            
         </div>
+            </ReactLenis>
     );
 }
 export default App;
