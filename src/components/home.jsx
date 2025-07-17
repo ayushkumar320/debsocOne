808dda8fd13fec25f52a67916ef7d6d40dc2686e
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-17 22:01:22 +0530

    tweak the home page design

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="home">
-        <div>
-          <img src="./media/M5.jpg" alt="group photo" className="shadow-md p-0 mt-10  h-[700px] relative left-0 w-full " />
-        </div>
+    <div className="relative h-[70vh] md:h-screen w-full overflow-hidden">
+
+      <img
+        src="./media/M5.jpg"
+        alt="group photo"
+        className="absolute inset-0 w-full h-full object-cover object-center"
+      />
+
+      <div className="absolute inset-0 bg-black/60 flex items-center justify-center">
+        <h1 className="animate-fadeUp text-4xl md:text-7xl font-extrabold text-orange-500 border-orange-500 px-4 text-decoration-none">
+          SMVIT DEBSOC
+        </h1>
+      </div>
     </div>
   );
 }
