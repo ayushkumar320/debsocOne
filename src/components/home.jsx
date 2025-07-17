481c0aad191523c2e1561b9afece4051a5ec99aa
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-17 21:45:41 +0530

    made navbar responsive

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,10 +3,8 @@ import React from "react";
 function Home() {
   return (
     <div className="home">
-        <h1 className="text-4xl font-bold  mt-10">Welcome to SMVIT DEBSOC</h1>
-
         <div>
-          <img src="./media/M5.jpg" alt="group photo" className="shadow-md p-0 mt-0  h-[700px] relative left-0 w-full " />
+          <img src="./media/M5.jpg" alt="group photo" className="shadow-md p-0 mt-10  h-[700px] relative left-0 w-full " />
         </div>
     </div>
   );
