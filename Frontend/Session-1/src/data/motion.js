3799960d6e30dd378bcc540e0ef957cae6690712
mobasshir <mobasshirkhan9931@gmail.com>
2025-07-18 18:44:35 +0530

    added more motion data and added new member data

--- Frontend/Session-1/src/data/motion.js
+++ Frontend/Session-1/src/data/motion.js
@@ -72,47 +72,47 @@ export const motion =[
     {
     motion: "The council is of the opinion that exclusive art spaces bring more harm than good.",
     InfoSlide: "Exclusive art spaces refer to exhibitions or installations that limit access to certain groups, usually based on identity. The Ladies' Lounge at the Museum of Old and New Art (MONA) in Tasmania, Australia, is an example of an exclusive art space that recently prohibited cisgender men from entering the exhibit. The museum subsequently faced a discrimination lawsuit, which led to the closure of the lounge.",
-    types: ["Art", "Culture", "LGBTQ+"]
+    types:  "Art Culture LGBTQ+"
   },
   {
     motion: "This house believes that the queer movement in Jamaica should oppose rather than support the police",
     InfoSlide: "Jamaica's anti-sodomy and public morality laws have long sanctioned violence and discrimination against queer people. In recent years, the queer movement in Jamaica and its advocacy groups (e.g., J-FLAG, TransWave) have grown more visible and influential. Although formal arrests are now rare and police sometimes provide limited protection for events like Pride, officers regularly intimidate and harass queer people.",
-    types: ["LGBTQ+", "Minority Communities", "Police"]
+    types: "LGBTQ+ Minority Communities Police"
   },
   {
     motion: "This House opposes the focus on coming out in LGBTQ+ media and popular culture.",
     InfoSlide: null,
-    types: ["Culture", "LGBTQ+", "Media"]
+    types: "Culture LGBTQ+ Media"
   },
   {
     motion: "As a reputable children's book author, this House would choose to queer code main characters in the story",
     InfoSlide: "Queer coding is the act of using subtext to code a character in media as queer. The process of queer coding entails applying characteristics associated with queerness to a character, without ever confirming it.",
-    types: ["Children", "LGBTQ+", "Social Policy"]
+    types: "Children LGBTQ+ Social Policy"
   },
   {
     motion: "This house believes that the feminist movement should actively oppose the participation of gender and sexual minorities in hip-hop/rap",
     InfoSlide: null,
-    types: ["Culture", "Feminism", "LGBTQ+"]
+    types: "Culture Feminism LGBTQ+"
   },
   {
     motion: "This house supports Western pop artists with significant queer fanbase actively rejecting to perform in countries with queer-oppressive social policies (e.g. Lizzo in Texas, Beyonce in UAE)",
     InfoSlide: null,
-    types: ["Culture", "International Relations", "LGBTQ+"]
+    types: "Culture International Relations LGBTQ+"
   },
   {
     motion: "That we support the research into allowing parents to genetically ensure that they will have an LGBT baby.",
     InfoSlide: null,
-    types: ["Social Policy", "LGBTQ+", "Children", "Science/Technology"]
+    types: "Social Policy LGBTQ+ Children Science/Technology"
   },
   {
     motion: "This house regrets mainstream media's popularization of Western drag culture",
     InfoSlide: null,
-    types: ["Culture", "LGBTQ+", "Media"]
+    types: "Culture LGBTQ+ Media"
   },
   {
     motion: "This house supports Western pop artists with significant queer fanbase actively rejecting to perform in countries with queer-oppressive social policies (e.g. Lizzo in Texas, Beyonce in UAE)",
     InfoSlide: null,
-    types: ["Culture", "International Relations", "LGBTQ+"]
+    types: "Culture International Relations LGBTQ+"
   }
 
 ]
