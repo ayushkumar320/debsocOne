5056b2369a4a405d09f8e21020c07307f3b04231
mobasshir <mobasshirkhan9931@gmail.com>
2025-07-18 08:53:18 +0530

    pushed code with bugs

---
+++ Frontend/Session-1/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Motion from "./Motion"
+
+function App() {
+  return (
+   <div className="min-h-screen bg-gray-800 text-white">
+     <Motion />
+   </div>
+  );
+}
+
+export default App;
