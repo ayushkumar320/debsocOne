34e8d9e2271cd07cb077850b443c64a5322ba32b
mobasshir <mobasshirkhan9931@gmail.com>
2025-07-18 23:11:20 +0530

    merged

--- Frontend/FormPage/vite.config.js
+++ Frontend/FormPage/vite.config.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-=======
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -14,4 +5,3 @@ export default defineConfig({
   plugins: [react()],
   base: '/FormPage/', // important so JS/CSS use correct paths
 });
->>>>>>> 27746fea2ce12bb3d21566277ec6af8fa8b680c8
