473b546471990712dcac84f688c22be04036b041
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-18 22:49:06 +0530

    fixed gallery

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -15,7 +15,7 @@ import M1 from "../../media/M1.jpg";
 import M2 from "../../media/M2.jpg";
 import M3 from "../../media/M3.jpg";
 import M4 from "../../media/M4.jpg";
-import M5 from "../../media/M5.jpg";
+import M7 from "../../media/M7.jpg";
 import M6 from "../../media/M6.jpg";
 export const galleryImages = [
   DPL1, DPL2, DPL3, DPL4, DPL5, DPL6,
